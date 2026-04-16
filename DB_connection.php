12cad84f4451ceef10ccbe42290feb0c433e9a4a
boussandel afef <boussandelafef@gmail.com>
2026-04-16 11:24:18 +0100

    Ajout de Dockerfile et docker-compose pour l'infrastructure as code

--- DB_connection.php
+++ DB_connection.php
@@ -1,14 +1,16 @@
-<?php  
+<?php
 
-$sName = "localhost";
-$uName = "root";
-$pass  = "";
-$db_name = "task_management_db";
+
+$sName = getenv('DB_HOST') ?: 'localhost';
+$uName = getenv('DB_USER') ?: 'root';
+$pass  = getenv('DB_PASSWORD') ?: '';
+$db_name = getenv('DB_NAME') ?: 'task_management_db';
 
 try {
-	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOExeption $e){
-	echo "Connection failed: ". $e->getMessage();
-	exit;
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){  // Notez : PDOException (pas Exeption)
+    echo "Connection failed: ". $e->getMessage();
+    exit;
 }
+?>
